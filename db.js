c96cc3951b5040578050eb908cf7ffba472fad1d
shruu10 <151463033+shruu10@users.noreply.github.com>
2024-09-09 18:10:53 +0530

    shift mongoDB Online v1.1

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 //Define MongoDB connection URL
-const mongoURL = 'mongodb://localhost:27017/hotel'
+// const mongoURL = process.env.MONGODB_URL_LOCAL;
+const mongoURL = process.env.MONGODB_URL;
+
 
 //Set up MongoDB connection
 mongoose.connect(mongoURL, {
